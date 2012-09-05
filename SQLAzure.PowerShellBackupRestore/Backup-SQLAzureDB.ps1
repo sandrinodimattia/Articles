fa079ffeeeecd6b28fc274ff5f914fcc4a1565e6
Sandrino Di Mattia <contact@sandrino.be>
2012-09-05 23:09:20 +0200

    Added backup/restore scripts for SQL Azure

---
+++ SQLAzure.PowerShellBackupRestore/Backup-SQLAzureDB.ps1
@@ -0,0 +1,26 @@
+﻿param([string]$ConnectionString = $(throw "The ConnectionString parameter is required."), 
+      [string]$DatabaseName = $(throw "The DatabaseName parameter is required."),
+      [string]$OutputFile = $(throw "The OutputFile parameter is required."), 
+      [string]$SqlInstallationFolder = "C:\Program Files (x86)\Microsoft SQL Server")
+      
+# Load DAC assembly.
+$DacAssembly = "$SqlInstallationFolder\110\DAC\bin\Microsoft.SqlServer.Dac.dll"
+Write-Host "Loading Dac Assembly: $DacAssembly"
+Add-Type -Path $DacAssembly
+Write-Host "Dac Assembly loaded."
+
+# Initialize Dac service.
+$now = $(Get-Date).ToString("HH:mm:ss")
+$Services = new-object Microsoft.SqlServer.Dac.DacServices $ConnectionString
+if ($Services -eq $null)
+{
+    exit
+}
+
+# Start the actual export.
+Write-Host "Starting backup at $DatabaseName at $now"
+$Watch = New-Object System.Diagnostics.StopWatch
+$Watch.Start()
+$Services.ExportBacpac($OutputFile, $DatabaseName)
+$Watch.Stop()
+Write-Host "Backup completed in" $Watch.Elapsed.ToString()
---
+++ SQLAzure.PowerShellBackupRestore/Restore-SQLAzureDB.ps1
@@ -0,0 +1,28 @@
+﻿param([string]$ConnectionString = $(throw "The ConnectionString parameter is required."), 
+      [string]$DatabaseName = $(throw "The DatabaseName parameter is required."),
+      [string]$InputFile = $(throw "The InputFile parameter is required."), 
+      [string]$SqlInstallationFolder = "C:\Program Files (x86)\Microsoft SQL Server")
+      
+# Load DAC assembly.
+$DacAssembly = "$SqlInstallationFolder\110\DAC\bin\Microsoft.SqlServer.Dac.dll"
+Write-Host "Loading Dac Assembly: $DacAssembly"
+Add-Type -Path $DacAssembly
+Write-Host "Dac Assembly loaded."
+
+# Initialize Dac service.
+$now = $(Get-Date).ToString("HH:mm:ss")
+$Services = new-object Microsoft.SqlServer.Dac.DacServices $ConnectionString
+if ($Services -eq $null)
+{
+    exit
+}
+
+# Start the actual export.
+Write-Host "Starting restore to $DatabaseName at $now"
+$Watch = New-Object System.Diagnostics.StopWatch
+$Watch.Start()
+$Package =  [Microsoft.SqlServer.Dac.BacPackage]::Load($InputFile)
+$Services.ImportBacpac($Package, $DatabaseName)
+$Package.Dispose()
+$Watch.Stop()
+Write-Host "Restore completed in" $Watch.Elapsed.ToString()
